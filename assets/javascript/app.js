f91a8e20905856af4c1e9642926471ab3fea4dfd
David Flores <drfloresj@gmail.com>
2020-03-02 21:42:54 -0800

    buttons load gifs, animation toggles, a little style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,24 +2,28 @@
 
 var myApiKey = "y5fd6BmElSM3UPPatTgJh0kmpD3Ug60X";
 var arrTopics = ["cat", "dog", "bunny", "bird"];
-const $buttonDiv = $("#button-container");
+const $buttonSection = $("#button-container");
+const $gifSection = $("#gif-container");
 
 // FUNCTIONS
 
 function makeButtons() {
     // create a button for each string in topic array
     for (topic of arrTopics) {
-        $buttonDiv.append($("<button>").attr({
-            class: "btn btn-primary px-3 m-2",
+        $buttonSection.append($("<button>").attr({
+            class: "btn btn-lg btn-primary px-3 m-2",
             id: arrTopics.indexOf(topic)
         }).text(topic));
     }
 }
 
+
 // INITIALIZE
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+// })
 
 makeButtons();
-console.log($buttonDiv);
 
 $("#main-container").on("click", ".btn", function () {
 
@@ -34,12 +38,40 @@ $("#main-container").on("click", ".btn", function () {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        // do stuff after getting back response 
         console.log(response);
-        console.log(response.data[0].url)
+        // do stuff after getting back response 
         for (gif of response.data) {
-            console.log("test: ", gif.url);
-        }
+            let $gifWrapper = $("<div>").attr({
+                "class": "d-inline-block text-left p-1"
+            });
+            let $img = $("<img/>").attr({
+                "id": response.data.indexOf(gif),
+                "class": "gif rounded",
+                "src": gif.images.fixed_height.url,
+                "alt": gif.title,
+                "giphy-rating": gif.rating,
+                "data-animate-url": gif.images.fixed_height.url,
+                "data-still-url": gif.images.fixed_height_still.url,
+                "data-state": "animated",
+                "title": gif.rating
+            });
 
+            $gifWrapper.append($("<p>").attr("class", "pb-1").text("Rating: " + gif.rating));
+            $gifWrapper.append($img);
+            $gifSection.prepend($gifWrapper);
+        }
     });
+});
+
+$("#gif-container").on("click", ".gif", function () {
+    let state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate-url"));
+        $(this).attr("data-state", "animated");
+    } else {
+        $(this).attr("src", $(this).attr("data-still-url"));
+        $(this).attr("data-state", "still");
+    }
+
 });
